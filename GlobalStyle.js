7728a076189e50efa0c8bb5ddc547e35ff3ebb85
annaleaschroeder <67912360+annaleaschroeder@users.noreply.github.com>
2020-11-07 16:22:00 +0100

    Initial commit, setup

---
+++ GlobalStyle.js
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  html {
+    height: 100%;
+  }
+
+  body {
+    font-family: sans-serif;
+    font-size: 112.5%;
+
+  }
+
+  #root {
+    display: grid;
+     // To add header, footer and scrolling middle part:
+     // @see https://codepen.io/neuefische/pen/PoZeqzQ
+    grid-template-rows: auto;
+    height: 100%;
+    left: 0;
+    margin: 0;
+    max-width: 600px;
+    position: fixed;
+    top: 0;
+    width: 100%;
+  }`;
